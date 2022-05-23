962e05e6e03b6073fdc5ad85ec829ae8f19b1647
임채성 <pan1129@naver.com>
2022-05-23 12:04:24 +0900

    평균구하기 Commit

---
+++ src/자료구조/평균구하기.java
@@ -0,0 +1,22 @@
+package 자료구조;
+
+import java.util.Scanner;
+
+public class 평균구하기 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int A[] = new int[N];
+        for(int i=0; i < N; i++){
+            A[i] = sc.nextInt();
+        }
+        long sum = 0;
+        long max = 0;
+        for(int i = 0; i < N; i++){
+            if(A[i] > max) max = A[i];
+            sum = sum + A[i];
+        }
+        System.out.println(sum * 100.0 / max / N);
+    }
+}
